64e5b4c58866e8cff4837d70a1751873845f2f9d
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-06-18 16:37:46 +0200

    handling the args, working on draw

--- prob_calculator.py
+++ prob_calculator.py
@@ -4,29 +4,37 @@ import random
 # mostly will be using the random module
 
 class Hat: 
-  def __init__(self, red=None, blue=None, yellow=None, green=None, orange=None, black=None, pink=None, striped=None):
-    self.red = red
-    self.blue = blue
-    self.blue = blue
-    self.yellow = yellow
-    self.green = green
-    self.orange = orange
-    self.black = black
-    self.pink = pink
-    self.striped = striped
+  # def __init__(self, red=None, blue=None, yellow=None, green=None, orange=None, black=None, pink=None, striped=None):
+  #   self.red = red
+  #   self.blue = blue
+  #   self.blue = blue
+  #   self.yellow = yellow
+  #   self.green = green
+  #   self.orange = orange
+  #   self.black = black
+  #   self.pink = pink
+  #   self.striped = striped
+  
+  def __init__(self, **colors):
+    self.contents = []
+    print(colors)
+    for k,v in colors.items():
+      for x in range(v):
+        self.contents.append(k)
+  
+  def draw(self, numOfBalls):
+    if numOfballs > len(self.contents):
+      return self.contents
 
-  def __str__(self):
-    if self.red:
-      string = f"{self.red} red balls"
-    return string
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
   def getstff():
     print('stff')
 
 
-newhat = Hat(red=2)
-print(newhat)
+newhat = Hat(red=2, blue=4)
+print(newhat.contents)
 
   
 
