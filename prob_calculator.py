96d17a3fddd7b8f23dbc21b0990d56fa6e636bab
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-06-18 13:11:50 +0200

    standard upload

---
+++ prob_calculator.py
@@ -0,0 +1,32 @@
+import copy
+import random
+# Consider using the modules imported above.
+# mostly will be using the random module
+
+class Hat: 
+  def __init__(self, red=None, blue=None, yellow=None, green=None, orange=None, black=None, pink=None, striped=None):
+    self.red = red
+    self.blue = blue
+    self.blue = blue
+    self.yellow = yellow
+    self.green = green
+    self.orange = orange
+    self.black = black
+    self.pink = pink
+    self.striped = striped
+
+  def __str__(self):
+    if self.red:
+      string = f"{self.red} red balls"
+    return string
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  def getstff():
+    print('stff')
+
+
+newhat = Hat(red=2)
+print(newhat)
+
+  
+
