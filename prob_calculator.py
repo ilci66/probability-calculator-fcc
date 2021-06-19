6a90b5311bbf9351b891278111b30df36cb696b6
ilci66 <77514750+ilci66@users.noreply.github.com>
2021-06-19 14:51:31 +0200

    working on the experiment function

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,50 +1,69 @@
 import copy
 import random
-# Consider using the modules imported above.
-# mostly will be using the random module
 
 class Hat: 
-  # def __init__(self, red=None, blue=None, yellow=None, green=None, orange=None, black=None, pink=None, striped=None):
-  #   self.red = red
-  #   self.blue = blue
-  #   self.blue = blue
-  #   self.yellow = yellow
-  #   self.green = green
-  #   self.orange = orange
-  #   self.black = black
-  #   self.pink = pink
-  #   self.striped = striped
   
   def __init__(self, **colors):
     self.contents = []
-    print(colors)
     for k,v in colors.items():
       for x in range(v):
         self.contents.append(k)
-  
-  # def draw(self, number):
-  #   if number > len(self.contents):
-  #     return self.contents
-  #   for x in range(number):
-  #     self.contents.remove(random.choice(self.contents))
-  #   return self.contents
 
   def draw(self, number):
     allPopped = []
     if number > len(self.contents):
       return self.contents
     for x in range(number):
-      popped  = self.contents.pop(random.randint(0,len(newhat.contents)))
+      popped  = self.contents.pop(random.randint(0,len(self.contents)-1))
       allPopped.append(popped)
     return allPopped
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 
-# with every draw done check if the allpopped list contains, if so add one to count and the divide the count to num_experiments
+  expected_balls_copy = copy.deepcopy(expected_balls)
+  hat_copy = copy.deepcopy(hat)
+  gotten_balls = hat_copy.draw(num_balls_drawn) 
+
   
 
+  count = 0
+  wantedballs = []
+  for k,v in expected_balls_copy.items():
+    print(str(k),v, range(v))
+    for i in range(int(v)):
+      wantedballs.append(k)
 
+  for i in range(num_experiments):
+    expected_balls_copy = copy.deepcopy(expected_balls)
+    hat_copy = copy.deepcopy(hat)
+    gotten_balls = hat_copy.draw(num_balls_drawn)
+
+    # for x in range(len(wantedballs)):
+# implement this logic here out of time for today
+# fruits1 = ['Mango','orange','jackfruit']
+# fruits2 = ['Mango','orange',]
+# # new_list=  all(item in fruits1 for item in fruits2)
+# # if new_list is True:
+# #     print("True")    
+# # else :
+# #     print("False")
+# count = 0
+# for x in range(len(fruits2)):
+#   if fruits2[0] in fruits1:
+#     print("yep")
+#     fruits2.remove(fruits2[0])
+#   if len(fruits2) == 0:
+#     count += 1 
+
+# print(fruits2, count)
+
+  # for i in range(num_experiments):
+  #   check = all(item in wantedballs for item in hat.contents)
+  #   if check:
+  #     count += 1
+
+  return count / num_experiments
 
 hat = Hat(black=6, red=4, green=3)
 print(hat.contents)
